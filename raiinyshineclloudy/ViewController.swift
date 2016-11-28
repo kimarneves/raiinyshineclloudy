17bf94e71852b37a65bb7da438a786a2696890b8
Kimar Arakaki Neves <kimar@kimar.eng.br>
2016-11-27 20:52:39 -0800

    Initial Commit

---
+++ raiinyshineclloudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  raiinyshineclloudy
+//
+//  Created by Kimar Arakaki Neves on 2016-11-27.
+//  Copyright © 2016 Kimar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
